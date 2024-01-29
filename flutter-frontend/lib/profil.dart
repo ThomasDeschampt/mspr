9daa001487440f8d61660891e8bf4ab4fdbb4003
William Quesnot <williamquesnot@MacBook-Air-de-William.local>
2024-01-29 13:45:02 +0100

    push tout

--- flutter-frontend/lib/profil.dart
+++ flutter-frontend/lib/profil.dart
@@ -1,19 +1,94 @@
 import 'package:flutter/material.dart';
 
-class ProfilPage extends StatelessWidget {
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mes Plantes',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const ProfilPage(),
+    );
+  }
+}
+
+class ProfilPage extends StatefulWidget {
   const ProfilPage({Key? key}) : super(key: key);
+
+  @override
+  _ProfilPageState createState() => _ProfilPageState();
+}
+
+class _ProfilPageState extends State<ProfilPage> {
+  final List<String> _plantes = ['Plante1', 'Plante2', 'Plante3'];
+  final List<String> _gardiennages = ['Plante1', 'Plante2', 'Plante3'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Mon profil'),
+        title: const Text('Accueil'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-        ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 120, // Hauteur du cercle
+            width: 120, // Largeur du cercle
+            child: CircleAvatar(
+              backgroundColor: Colors.blue, // Couleur du cercle
+              radius: 60, // Rayon du cercle
+              child: Icon(
+                Icons.person, // Icône par défaut (vous pouvez le modifier)
+                size: 80, // Taille de l'icône
+                color: Colors.white, // Couleur de l'icône
+              ),
+            ),
+          ),
+          const SizedBox(height: 16), // Espacement entre le cercle et le texte
+          const Text(
+            'Nom Prenom',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 16), // Espacement entre le texte et la liste de plantes
+          Container(
+            padding: const EdgeInsets.all(16),
+            child: const Text(
+              'Mes plantes :',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _plantes.length,
+              itemBuilder: (context, index) {
+                return ListTile(title: Text(_plantes[index]));
+              },
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(16),
+            child: const Text(
+              'Mes gardiennages :',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _gardiennages.length,
+              itemBuilder: (context, index) {
+                return ListTile(title: Text(_gardiennages[index]));
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
--- flutter-frontend/lib/trouver.dart
+++ flutter-frontend/lib/trouver.dart
@@ -1,19 +1,33 @@
 import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
+import "package:flutter_map/flutter_map.dart";
+import "package:http/http.dart" as http;
+import "dart:convert" as convert;
 
 class TrouverPage extends StatelessWidget {
   const TrouverPage({Key? key}) : super(key: key);
+  final String apiKey = "r5tgib4GBlr2Zv5Cj8Zla0RGYWZRpZOw";
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Trouver un gardiennage'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-        ),
+    return MaterialApp(
+      title: "TomTom Map",
+      home: Scaffold(
+        body: Center(
+            child: Stack(
+          children: <Widget>[
+            FlutterMap(
+              options: new MapOptions(zoom: 13.0),
+              layers: [
+                new TileLayerOptions(
+                  urlTemplate: "https://api.tomtom.com/map/1/tile/basic/main/"
+                      "{z}/{x}/{y}.png?key={apiKey}",
+                  additionalOptions: {"apiKey": apiKey},
+                )
+              ],
+            )
+          ],
+        )),
       ),
     );
   }
