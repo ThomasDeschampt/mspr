973377053b2cd04aab60793c81421df9c258ce6e
William Quesnot <williamquesnot@MacBook-Air-de-William.local>
2024-02-28 11:20:07 +0100

    modif botanisteRoutes

--- express-backend/routes/botanisteRoutes.js
+++ express-backend/routes/botanisteRoutes.js
@@ -16,11 +16,18 @@ router.post("/ajouterBotaniste", async (req, res) => {
 router.get("/estBotaniste", async (req, res) => {
   const { psd_utl } = req.query;
   try {
-    await estBotaniste(psd_utl);
+    botaniste = await estBotaniste(psd_utl);
+    if (botaniste) {
+      console.log("botaniste trouvé");
+      res.status(200).json({ message: "botaniste trouvé" });
+    } else {
+      console.log("botaniste non trouvé");
+      res.status(404).json({ message: "botaniste non trouvé" });
+    }
   } catch (erreur) {
-    console.error("Erreur lors de l'ajout de l'Botaniste:", erreur.message);
+    console.error("Erreur lors de la verification du botaniste:", erreur.message);
     res.status(500).json({
-      message: "Erreur lors de l'ajout de l'Botaniste",
+      message: "Erreur lors de la verification du botaniste",
       erreur: erreur.message,
     });
   }
