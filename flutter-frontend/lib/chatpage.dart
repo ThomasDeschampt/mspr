4019917127575e1a4d85bd242ac12b4e53f16397
Faris Maisonneuve <91095075+faris26@users.noreply.github.com>
2024-03-02 18:41:21 +0100

    Chatpage add + message.dart modifié

--- flutter-frontend/lib/chatpage.dart
+++ flutter-frontend/lib/chatpage.dart
@@ -2,17 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-
 class chatpage extends StatefulWidget {
-  final String myId;
-  final String otherUserId;
-  final String otherUserPseudo;
+  final String myPseudo;
+  final String proprietaire;
+  final String gardien;
 
   const chatpage({
     Key? key,
-    required this.myId,
-    required this.otherUserId,
-    required this.otherUserPseudo, 
+    required this.myPseudo,
+    required this.proprietaire,
+    required this.gardien,
   }) : super(key: key);
 
   @override
@@ -28,59 +27,63 @@ class _chatpageState extends State<chatpage> {
     loadMessages();
   }
 
-  Future<void> loadMessages() async {
-    final uri = Uri.parse('http://15.237.169.255:3000/api/message/afficher');
-    final response = await http.get(uri, headers: {
-      'id_utl': widget.myId,
-      'id_utl_1': widget.otherUserId,
-    });
+ Future<void> loadMessages() async {
+  // Construisez l'URI avec les paramètres de la query
+final uri = Uri.http('15.237.169.255:3000', '/api/message/afficher', {
+  'psd_utl': widget.myPseudo == 'gardien' ? 'proprietaire' : 'gardien',
+  'psd_utl_1': widget.proprietaire.toString(),
+});
 
-    if (response.statusCode == 200) {
-      List<dynamic> messagesJson = json.decode(response.body);
-      setState(() {
-      });
-    } else {
-      print('failed to load messages');
-    }
-  }
-
- Future<void> sendMessage() async {
-  if (messageController.text.isEmpty) {
-    // Pas de message à envoyer
-    return;
-  }
-  final uri = Uri.parse('http://15.237.169.255:3000/api/message/ajouter');
-  final headers = {"Content-Type": "application/x-www-form-urlencoded"};
-  final body = {
-    'txt_msg': messageController.text,  // Utilisez le texte saisi par l'utilisateur
-    'exp_msg': widget.myId,             // ID de l'utilisateur courant qui envoie le message
-    'id_utl': widget.myId,              // ID de l'utilisateur courant (peut être le même que exp_msg)
-    'id_utl_1': widget.otherUserId,     // ID de l'autre utilisateur avec qui vous avez une conversation
-  };
-
-  final response = await http.post(uri, headers: headers, body: body);
+  // Effectuez la requête GET
+  final response = await http.get(uri); // Les headers ne sont pas nécessaires ici
 
   if (response.statusCode == 200) {
-    // Le message a été envoyé avec succès
-    messageController.clear();
-    loadMessages(); // Recharger les messages pour afficher le nouveau message
+    List<dynamic> messagesJson = json.decode(response.body);
+    setState(() {});
   } else {
-    // Gérer l'échec de l'envoi du message
-    print('Failed to send message: ${response.body}');
+    print('failed to load messages');
   }
 }
 
+  Future<void> sendMessage() async {
+    if (messageController.text.isEmpty) {
+      // Pas de message à envoyer
+      return;
+    }
+    final uri = Uri.http('15.237.169.255:3000', '/api/message/ajouter', {
+      'txt_msg': messageController.text,
+      'exp_msg': widget.myPseudo.toString(),
+      'psd_utl': widget.gardien.toString(),
+      'psd_utl_1': widget.proprietaire.toString(),
+    });
+
+   final response = await http.post(uri);
+
+    if (response.statusCode == 200) {
+      // Le message a été envoyé avec succès
+      messageController.clear();
+      loadMessages(); // Recharger les messages pour afficher le nouveau message
+    } else {
+      // Gérer l'échec de l'envoi du message
+      print('Failed to send message: ${response.body}');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.otherUserPseudo), 
-      ),
+     appBar: AppBar(
+    title: Text(
+      widget.myPseudo == widget.proprietaire ? widget.gardien :
+      widget.myPseudo == widget.gardien ? widget.proprietaire :
+      'Pas de conversation', // Fallback au cas où aucune des conditions n'est remplie
+  ),
+),
       body: Column(
         children: [
           Expanded(
-            child: ListView.builder(itemBuilder: (BuildContext context, int index) {  },
+            child: ListView.builder(
+              itemBuilder: (BuildContext context, int index) {},
               // Afficher les messages ici
             ),
           ),
@@ -98,7 +101,7 @@ class _chatpageState extends State<chatpage> {
                 ),
                 IconButton(
                   icon: Icon(Icons.send),
-                    onPressed: sendMessage,  
+                  onPressed: sendMessage,
                 ),
               ],
             ),
