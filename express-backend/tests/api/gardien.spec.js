1901f60a6c1ec58029619d7693209f51a3686743
williamqsnt <william.quesnot@ecoles-epsi.net>
2024-03-09 16:14:06 +0100

    ajustement test unitaires

--- express-backend/tests/api/gardien.spec.js
+++ express-backend/tests/api/gardien.spec.js
@@ -1,44 +1,64 @@
-const { ajouterGardien } = require("../../api/Gardien");
+const { ajouterGardien, verifierGardien } = require("../../api/Gardien");
 const Gardien = require("../../models/Gardien");
+const Utilisateur = require("../../models/Utilisateur");
+
+jest.mock("../../models/Utilisateur", () => ({
+  findOne: jest.fn(),
+}));
 
 jest.mock("../../models/Gardien", () => ({
   create: jest.fn(),
+  findOne: jest.fn(),
 }));
 
-describe("Test de la fonction ajouterGardien", () => {
+describe("Tests unitaires pour les fonctions API Gardien", () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it("Devrait ajouter un nouveau gardien", async () => {
-    const nouveauGardienMock = {
-      id_utl: 1,
-      toJSON: jest.fn(() => ({ id_utl: 1 })),
-    };
+  describe("Test de la fonction ajouterGardien", () => {
+    it("Devrait ajouter un nouveau gardien", async () => {
+      Utilisateur.findOne.mockResolvedValue({ id_utl: 1 });
 
-    Gardien.create.mockResolvedValue(nouveauGardienMock);
+      Gardien.create.mockResolvedValue({ id_gdn: 1 });
 
-    const spy = jest.spyOn(console, "log");
+      await ajouterGardien(123);
 
-    await ajouterGardien(1);
+      expect(Utilisateur.findOne).toHaveBeenCalledWith({
+        where: { psd_utl: 123 },
+      });
 
-    expect(Gardien.create).toHaveBeenCalledWith({ id_utl: 1 });
-
-    expect(spy).toHaveBeenCalledWith("Nouveau gardien ajouté:", { id_utl: 1 });
+      expect(Gardien.create).toHaveBeenCalledWith({ id_utl: 1 });
+    });
   });
 
-  it("Devrait afficher une erreur en cas de problème", async () => {
-    const erreur = new Error("Erreur lors de la création du gardien");
+  describe("Test de la fonction verifierGardien", () => {
+    it("Devrait retourner true si un gardien est trouvé", async () => {
+      Utilisateur.findOne.mockResolvedValue({ id_utl: 1 });
 
-    Gardien.create.mockRejectedValue(erreur);
+      Gardien.findOne.mockResolvedValue({ id_gdn: 1 });
 
-    console.error = jest.fn();
+      const result = await verifierGardien(123);
 
-    await ajouterGardien(1);
+      expect(result).toBe(true);
+    });
 
-    expect(console.error).toHaveBeenCalledWith(
-      "Erreur lors de l'ajout du gardien:",
-      erreur.message,
-    );
+    it("Devrait retourner false si aucun gardien n'est trouvé", async () => {
+      Utilisateur.findOne.mockResolvedValue({ id_utl: 1 });
+
+      Gardien.findOne.mockResolvedValue(null);
+
+      const result = await verifierGardien(123);
+
+      expect(result).toBe(false);
+    });
+
+    it("Devrait retourner false si aucun utilisateur n'est trouvé", async () => {
+      Utilisateur.findOne.mockResolvedValue(null);
+
+      const result = await verifierGardien(123);
+
+      expect(result).toBe(false);
+    });
   });
 });
