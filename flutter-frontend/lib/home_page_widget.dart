8786bce46444bda2086ea8d230d97e2c75900740
Enzo Perbet <101737157+EnzoPerbet@users.noreply.github.com>
2024-06-18 10:00:43 +0200

    Update home_page_widget.dart

--- flutter-frontend/lib/home_page_widget.dart
+++ flutter-frontend/lib/home_page_widget.dart
@@ -7,6 +7,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 import 'home_page_model.dart';
 export 'home_page_model.dart';
