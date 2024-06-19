0aa8116a81ffc33319df12091214e9f06172fa37
williamqsnt <william.quesnot@ecoles-epsi.net>
2024-06-19 09:59:35 +0200

    ajout swagger utilisateur/conversation/message

--- express-backend/swagger.js
+++ express-backend/swagger.js
@@ -1,28 +1,12 @@
-const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+const yaml = require('js-yaml');
+const fs = require('fs');
+const path = require('path');
 
-const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'API Documentation',
-      version: '1.0.0',
-      description: 'Documentation of the API endpoints'
-    },
-    servers: [
-      {
-        url: 'http://localhost:3000',
-        description: 'Local server'
-      }
-    ]
-  },
-  apis: ['./routes/*.js'], // Chemin vers vos fichiers de routes pour documenter les endpoints
-};
-
-const swaggerSpec = swaggerJSDoc(options);
+const swaggerDocument = yaml.load(fs.readFileSync(path.join(__dirname, 'swagger.yaml'), 'utf8'));
 
 const setupSwagger = (app) => {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 };
 
 module.exports = setupSwagger;
